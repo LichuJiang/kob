71b9679ca13ede059240286c3efd2190473f71e8
lichu <lj2231@nyu.edu>
2022-12-07 16:06:56 -0500

    bot CRUD

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -6,9 +6,82 @@
 
 <script>
 import ContentField from '../../../components/ContentField.vue'
+import $ from 'jquery'
+import {useStore} from 'vuex'
 export default {
     components: {
         ContentField
+    },
+    setup() {
+        const store = useStore();
+        $.ajax({
+            url:"http://127.0.0.1:3000/user/bot/getlist/",
+            type:"get",
+            headers: {
+                Authorization: "Bearer " + store.state.user.token,
+            },
+            success(resp) {
+                console.log(resp);
+            },
+            error(resp) {
+                console.log(resp);
+            }
+        })
+        // const store = useStore();
+        // $.ajax({
+        //     url:"http://127.0.0.1:3000/user/bot/update/",
+        //     type:"post",
+        //     data: {
+        //         bot_id:7,
+        //         title: "bot title7",
+        //         description: "bot description7",
+        //         content: "bot code7",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+        // $.ajax({
+        //     url:"http://127.0.0.1:3000/user/bot/add/",
+        //     type:"post",
+        //     data: {
+        //         title: "bot title",
+        //         description: "bot description",
+        //         content: "bot code",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+        // const store = useStore();
+        // $.ajax({
+        //     url:"http://127.0.0.1:3000/user/bot/remove/",
+        //     type:"post",
+        //     data: {
+        //         bot_id: 4,
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
     }
 }
 </script>
